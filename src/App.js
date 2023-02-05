04bc79736df65fb04976da7c9d4062afae1b2681
LeeSeungHun <dos07008@naver.com>
2023-02-05 19:03:19 +0900

    hello world api 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+
 function App() {
-  return <div>as</div>;
+  const handleButtonClick = async () => {
+    const result = await axios.get("http://localhost:8080/hello-world");
+    console.log(result.data);
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Hello World 호출</button>
+    </div>
+  );
 }
 
 export default App;
