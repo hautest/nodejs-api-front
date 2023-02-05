bdb675a1036cc1ca7c1201f7b3b4e93b37b75424
LeeSeungHun <dos07008@naver.com>
2023-02-06 00:57:01 +0900

    post 사용해보기

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ function App() {
     console.log(result.data);
   };
 
+  const handlePostButtonClick = async () => {
+    const result = await axios.post("http://localhost:8080/post", {
+      value: "value11",
+    });
+    console.log(result);
+  };
+
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
       <button onClick={handleButtonClick}>Hello World 호출</button>
+      <button onClick={handlePostButtonClick}>Post 버튼</button>
     </div>
   );
 }
