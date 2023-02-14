a54a5ab4dba40f1a5cc4a996936bb363cabe2ae0
LeeSeungHun <dos07008@naver.com>
2023-02-15 00:51:05 +0900

    mysql 연동, todolist 만들기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const handleButtonClick = async () => {
-    const result = await axios.get("http://localhost:8080/hello-world");
-    console.log(result.data);
+  const [list, setList] = useState([]);
+  const inputRef = useRef(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await axios.post("http://localhost:8080/create-list", {
+      value: inputRef.current.value,
+    });
+    setList(result.data);
+    inputRef.current.value = "";
   };
 
-  const handlePostButtonClick = async () => {
-    const result = await axios.post("http://localhost:8080/post", {
-      value: "value11",
+  const handleCheck = async (id) => {
+    const result = await axios.patch("http://localhost:8080/update-list", {
+      id,
     });
-    console.log(result);
+    setList(result.data);
   };
+
+  const handleDelete = async (id) => {
+    const result = await axios.delete(`http://localhost:8080/list/${id}`);
+    setList(result.data);
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const result = await axios.get("http://localhost:8080/list");
+      setList(result.data);
+    };
+    init();
+  }, []);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
-      <button onClick={handleButtonClick}>Hello World 호출</button>
-      <button onClick={handlePostButtonClick}>Post 버튼</button>
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <input ref={inputRef} />
+        <button>만들기</button>
+      </form>
+      <ul>
+        {list.map((item) => (
+          <li
+            key={item.id}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              cursor: "pointer",
+            }}
+          >
+            <span
+              style={{
+                textDecoration: item.checked ? "line-through" : "none",
+              }}
+              onClick={() => handleCheck(item.id)}
+            >
+              {item.value}
+            </span>
+            <button onClick={() => handleDelete(item.id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
